8b59e3c8898a1f99d65a5d13e6c578e6822f4dcc
dove209 <dove209@gmail.com>
2022-02-25 16:02:20 +0900

    Blockahin Creating a Block

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
-console.log('test');
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+const genesisBlock = new Block(0, '20202031', '', 'Hello', 123456);
+let blockchain = [genesisBlock];
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
