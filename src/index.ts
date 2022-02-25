a286b592dcdf98a673928944eea069ed8740df53
dove209 <dove209@gmail.com>
2022-02-25 16:35:47 +0900

    간단한 블록체인 구현

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
+    static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => {
+        return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    }
+
+    static validateStructure = (aBlock: Block): boolean => {
+       return typeof aBlock.index === "number" && 
+       typeof aBlock.hash === "string" &&
+       typeof aBlock.previousHash === "string" &&
+       typeof aBlock.timestamp === 'number' &&
+       typeof aBlock.data === "string"
+    }
+
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+
     constructor(
         index: number,
         hash: string,
@@ -23,6 +39,47 @@ const genesisBlock:Block = new Block(0, '20202031', '', 'Hello', 123456);
 
 let blockchain: Block[] = [genesisBlock];
 
+const getBlockchain = () : Block[] => blockchain;
+
+const getLastestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string) : Block => {
+    const previousBlock: Block = getLastestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data)
+    const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    addBlock(newBlock);
+    return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
 
 
-console.log(blockchain);
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false
+    } else if(previousBlock.index + 1 !== candidateBlock.index) {
+        return false
+    } else if(previousBlock.hash !== candidateBlock.previousHash) {
+        return false
+    } else if(getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        return false
+    } else {
+        return true
+    }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+    if(isBlockValid(candidateBlock, getLastestBlock())){
+        blockchain.push(candidateBlock);
+    }
+}
+
+createNewBlock('second blcok');
+createNewBlock('third blcok');
+createNewBlock('fourth blcok');
+
+console.log(blockchain)
